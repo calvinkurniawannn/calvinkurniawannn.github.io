d88b8a79ba18a3b6e3149ada9bc0b6dce601bb19
Calvin Kurniawan <79632981+calvinkurniawannn@users.noreply.github.com>
2022-07-18 11:13:57 +0700

    added

---
+++ script.js
@@ -0,0 +1,53 @@
+// Menus
+const menus = document.querySelector(".menus");
+const navbar = document.querySelector(".navbar");
+
+menus.addEventListener("click", () => {
+  menus.classList.toggle("change");
+  navbar.classList.toggle("change");
+});
+
+// End Of Menus
+
+// Section 4 toggle organizations
+
+document.querySelectorAll(".organizations-btn").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.classList.toggle("change");
+    btn.nextElementSibling.classList.toggle("change");
+  });
+});
+// Section 4 toggle organizations
+
+// Carousel section 4 projects
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("projects-content");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+// Carousel section 4 projects
